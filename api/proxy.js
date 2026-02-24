bb6c1728440a4f05cae422c1f99fec8f33c3b1da
SAKDEV-new <innovision72@gmail.com>
2026-02-24 13:29:33 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -7,17 +7,26 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(url, {
+      method: "GET",
       headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": url
-      }
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "*/*",
+        "Referer": url,
+        "Origin": url
+      },
     });
 
+    if (!response.ok) {
+      return res.status(response.status).send("Stream source error");
+    }
+
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", response.headers.get("content-type") || "application/vnd.apple.mpegurl");
 
     response.body.pipe(res);
 
   } catch (err) {
-    res.status(500).send("Error fetching stream");
+    res.status(500).send("Proxy fetch failed");
   }
 }
